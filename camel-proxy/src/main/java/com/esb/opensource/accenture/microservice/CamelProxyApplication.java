f9854676ce56a6547c323461e818b4091d6d0517
Chakraborty <r.a.chakraborty@accenture.com>
2017-02-16 01:38:11 +0530

    Camel-Proxy microservice using Camel, SpringBoot

---
+++ camel-proxy/src/main/java/com/esb/opensource/accenture/microservice/CamelProxyApplication.java
@@ -0,0 +1,12 @@
+package com.esb.opensource.accenture.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamelProxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamelProxyApplication.class, args);
+	}
+}
---
+++ camel-proxy/src/main/java/com/esb/opensource/accenture/microservice/ServiceCamelRoute.java
@@ -0,0 +1,72 @@
+package com.esb.opensource.accenture.microservice;
+
+import java.net.URI;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.LoggingLevel;
+import org.apache.camel.Processor;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.http.common.HttpOperationFailedException;
+import org.apache.camel.model.rest.RestBindingMode;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@Component
+public class ServiceCamelRoute extends RouteBuilder {
+
+	@Value("${rest.host}")
+	String host;
+	@Value("${rest.port}")
+	String port;
+	@Value("${rest.target.url}")
+	String EMPLYEE_SERVICE;
+
+	@Override
+	public void configure() throws Exception {
+
+		onException(HttpOperationFailedException.class).handled(false).end();
+
+		restConfiguration().component("jetty").host(host).port(port)
+				.bindingMode(RestBindingMode.json);
+
+		rest("/employee").get("/{id}").outType(EmployeeDetails.class)
+				.to("direct:getDetail");
+
+		from("direct:getDetail").process(new Processor() {
+
+			@Override
+			public void process(Exchange exchange) throws Exception {
+				String empId = (String) exchange.getIn().getHeader("id");
+				StringBuilder sb = new StringBuilder(EMPLYEE_SERVICE);
+				sb.append(empId);
+				sb.append("?bridgeEndpoint=true");
+				URI uri = new URI(sb.toString());
+				exchange.setProperty("employeeURI", uri);
+			}
+		}).removeHeaders("*").process(exchange -> {
+			exchange.getIn().setBody(null);
+		}).setHeader(Exchange.HTTP_METHOD, constant("GET"))
+				.toD("${property.employeeURI}").to("direct:result");
+
+		from("direct:result")
+				.streamCaching()
+				.log(LoggingLevel.INFO, "${body}")
+				.process(
+						exchange -> {
+							String jsonResponse = exchange.getIn().getBody(
+									String.class);
+							ObjectMapper objectMapper = new ObjectMapper();
+							/*
+							 * objectMapper.configure(SerializationFeature.
+							 * FAIL_ON_EMPTY_BEANS, false);
+							 * objectMapper.configure(Feature.AUTO_CLOSE_SOURCE,
+							 * true);
+							 */
+							EmployeeDetails emp = objectMapper.readValue(
+									jsonResponse, EmployeeDetails.class);
+							exchange.getIn().setBody(emp);
+						});
+	}
+}
